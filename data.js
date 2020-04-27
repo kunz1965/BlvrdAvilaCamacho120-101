e0f797afc567528e2c64a584bf642605be0a9e6c
kunz1965 <63274896+kunz1965@users.noreply.github.com>
2020-04-27 15:25:34 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,827 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fachada",
+      "name": "Fachada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.11433631230860897,
+        "pitch": -0.08401168325711339,
+        "fov": 1.3491744678877828
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8694595455565057,
+          "pitch": 0.20272435116048015,
+          "rotation": 0,
+          "target": "1-pb-acceso"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3303597715725708,
+          "pitch": -0.3709780552321025,
+          "title": "Local-Bodega con oficinas en RENTA",
+          "text": "Blvrd.&nbsp; Manuel Ávila Camacho <br>#120 -101"
+        },
+        {
+          "yaw": -1.2741596517729334,
+          "pitch": 0.23955259108057447,
+          "title": "Kunz y Asociados",
+          "text": "Administración Bienes Inmuebles"
+        },
+        {
+          "yaw": -1.2332621656877372,
+          "pitch": 0.24228092053504824,
+          "title": "Kunz y Asociados",
+          "text": "Administración Bienes Inmuebles"
+        }
+      ]
+    },
+    {
+      "id": "1-pb-acceso",
+      "name": "PB-Acceso",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.2582653904341594,
+        "pitch": 0.23014425717970965,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05992912438916065,
+          "pitch": 0.17685000612837776,
+          "rotation": 1.5707963267948966,
+          "target": "2-pb-vista2"
+        },
+        {
+          "yaw": 2.556820215031804,
+          "pitch": 0.2704532232913941,
+          "rotation": 0,
+          "target": "0-fachada"
+        },
+        {
+          "yaw": -2.599688830443114,
+          "pitch": 0.37175591562954047,
+          "rotation": 7.0685834705770345,
+          "target": "8-pa-recepcin"
+        },
+        {
+          "yaw": 1.2825228580519177,
+          "pitch": 0.49925322171385744,
+          "rotation": 0,
+          "target": "5-pb-oficina2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5853448408589408,
+          "pitch": -0.06852792082994696,
+          "title": "Amplio espacio, 411 m2",
+          "text": "En 2 niveles<div>Puede ser Bodega, Local, con oficinas</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-pb-vista2",
+      "name": "PB-Vista2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14284084710227418,
+          "pitch": 0.3108939725409918,
+          "rotation": 0,
+          "target": "3-pb-vista3"
+        },
+        {
+          "yaw": 0.5622793668066315,
+          "pitch": 0.42992689579528154,
+          "rotation": 0.7853981633974483,
+          "target": "4-pb-oficina1"
+        },
+        {
+          "yaw": 1.741582464344794,
+          "pitch": 0.21181569655775512,
+          "rotation": 0,
+          "target": "1-pb-acceso"
+        },
+        {
+          "yaw": 1.8320680704602212,
+          "pitch": 0.02137512367596095,
+          "rotation": 0.7853981633974483,
+          "target": "8-pa-recepcin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pb-vista3",
+      "name": "PB-Vista3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 1.3904307300272798,
+        "pitch": 0.3708949225866949,
+        "fov": 1.4119088925138374
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8806910519630975,
+          "pitch": 0.2862066680941098,
+          "rotation": 0,
+          "target": "2-pb-vista2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-pb-oficina1",
+      "name": "PB-Oficina1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -2.815532437507395,
+        "pitch": 0.6731754614537664,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0473843201494226,
+          "pitch": 0.43959881287201874,
+          "rotation": 0,
+          "target": "2-pb-vista2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-pb-oficina2",
+      "name": "PB-Oficina2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 1.5362935900796986,
+        "pitch": 0.37792109600037094,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6358763911323333,
+          "pitch": 0.43154809685061046,
+          "rotation": 0,
+          "target": "1-pb-acceso"
+        },
+        {
+          "yaw": 2.3467623633665866,
+          "pitch": 0.4931955759263875,
+          "rotation": 5.497787143782138,
+          "target": "7-pb-oficina4"
+        },
+        {
+          "yaw": 0.5385815975745807,
+          "pitch": 0.719882694143898,
+          "rotation": 11.780972450961727,
+          "target": "6-pb-oficina3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pb-oficina3",
+      "name": "PB-Oficina3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 2.4203065076545753,
+        "pitch": 0.2834379405050722,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.063854827264887,
+          "pitch": 0.3918167348548458,
+          "rotation": 0,
+          "target": "5-pb-oficina2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-pb-oficina4",
+      "name": "PB-Oficina4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -2.473535200652055,
+        "pitch": 0.4044993911364472,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.291264414507175,
+          "pitch": 0.7069996073694789,
+          "rotation": 1.5707963267948966,
+          "target": "5-pb-oficina2"
+        },
+        {
+          "yaw": -2.4000589131403096,
+          "pitch": 0.5008496278654402,
+          "rotation": 0,
+          "target": "1-pb-acceso"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-pa-recepcin",
+      "name": "PA-Recepción",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 1.3807179706812285,
+        "pitch": 0.4239901090762057,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0080268147939133,
+          "pitch": 0.416087658805127,
+          "rotation": 5.497787143782138,
+          "target": "1-pb-acceso"
+        },
+        {
+          "yaw": -1.0186875690782706,
+          "pitch": 0.49249857800902674,
+          "rotation": 6.283185307179586,
+          "target": "9-pa-accesobaos"
+        },
+        {
+          "yaw": -0.6984590938216595,
+          "pitch": 0.5913630503343263,
+          "rotation": 7.0685834705770345,
+          "target": "11-pa-oficina1"
+        },
+        {
+          "yaw": 0.948419094852337,
+          "pitch": 0.4187555798474172,
+          "rotation": 5.497787143782138,
+          "target": "12-pa-oficina2"
+        },
+        {
+          "yaw": 1.4181259252361595,
+          "pitch": 0.4829046954310421,
+          "rotation": 0.7853981633974483,
+          "target": "16-pa-oficina6"
+        },
+        {
+          "yaw": 1.1304209462634986,
+          "pitch": 0.2942603592473141,
+          "rotation": 5.497787143782138,
+          "target": "13-pa-oficina3"
+        },
+        {
+          "yaw": 1.2643630205833443,
+          "pitch": 0.17992924835030166,
+          "rotation": 5.497787143782138,
+          "target": "14-pa-oficina4"
+        },
+        {
+          "yaw": 1.3889263946049368,
+          "pitch": 0.26472627323691533,
+          "rotation": 0.7853981633974483,
+          "target": "15-pa-oficina5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-pa-accesobaos",
+      "name": "PA-AccesoBaños",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 2.309834676314593,
+        "pitch": 0.3951689373793297,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.60891232979645,
+          "pitch": 0.6678292061628817,
+          "rotation": 0.7853981633974483,
+          "target": "8-pa-recepcin"
+        },
+        {
+          "yaw": 2.643300239549271,
+          "pitch": 0.9111153657146556,
+          "rotation": 11.780972450961727,
+          "target": "11-pa-oficina1"
+        },
+        {
+          "yaw": -1.263643651421269,
+          "pitch": 0.8669742370808358,
+          "rotation": 7.0685834705770345,
+          "target": "10-pa-baos1"
+        },
+        {
+          "yaw": 3.032948766204986,
+          "pitch": 0.3047622901792586,
+          "rotation": 17.27875959474387,
+          "target": "12-pa-oficina2"
+        },
+        {
+          "yaw": -3.0555471457474326,
+          "pitch": 0.251566494490584,
+          "rotation": 20.420352248333668,
+          "target": "16-pa-oficina6"
+        },
+        {
+          "yaw": 3.0502657496765657,
+          "pitch": 0.16494932381655047,
+          "rotation": 10.995574287564278,
+          "target": "13-pa-oficina3"
+        },
+        {
+          "yaw": -3.071379824374967,
+          "pitch": 0.08729568628822193,
+          "rotation": 7.853981633974483,
+          "target": "15-pa-oficina5"
+        },
+        {
+          "yaw": 3.0678643741350804,
+          "pitch": 0.030891466035601667,
+          "rotation": 10.995574287564278,
+          "target": "13-pa-oficina3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-pa-baos1",
+      "name": "PA-Baños1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.6077506009611255,
+        "pitch": 0.8902893882974059,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0733195397618696,
+          "pitch": 0.922670347205651,
+          "rotation": 0,
+          "target": "9-pa-accesobaos"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-pa-oficina1",
+      "name": "PA-Oficina1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -2.5990237929850686,
+        "pitch": 0.7169748202923483,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1224291881024833,
+          "pitch": 0.6337318067625723,
+          "rotation": 5.497787143782138,
+          "target": "8-pa-recepcin"
+        },
+        {
+          "yaw": -2.6998317243248717,
+          "pitch": 0.7556339603839675,
+          "rotation": 0.7853981633974483,
+          "target": "9-pa-accesobaos"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-pa-oficina2",
+      "name": "PA-Oficina2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -2.2475674686966407,
+        "pitch": 0.27375053748744627,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3906145796984504,
+          "pitch": 0.6965296253194282,
+          "rotation": 0.7853981633974483,
+          "target": "8-pa-recepcin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-pa-oficina3",
+      "name": "PA-Oficina3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -2.447736891794939,
+        "pitch": 0.622627308860995,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8005930741471,
+          "pitch": 0.6177313720677748,
+          "rotation": 0.7853981633974483,
+          "target": "8-pa-recepcin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-pa-oficina4",
+      "name": "PA-Oficina4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -2.2937345228442183,
+        "pitch": 0.29070853538490127,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.485057273338052,
+          "pitch": 0.5901716165176616,
+          "rotation": 0,
+          "target": "8-pa-recepcin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-pa-oficina5",
+      "name": "PA-Oficina5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -2.710566931008909,
+        "pitch": 0.49662708128253286,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6399502353543163,
+          "pitch": 0.5923732398550801,
+          "rotation": 0.7853981633974483,
+          "target": "8-pa-recepcin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-pa-oficina6",
+      "name": "PA-Oficina6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -2.9973782520287315,
+        "pitch": 0.6259239459926462,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.445941827273593,
+          "pitch": 0.7491412151699457,
+          "rotation": 0.7853981633974483,
+          "target": "8-pa-recepcin"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "BlvrdAvilaCamacho120-101",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
